4156f450bc65d41eb51fa9964210bada38cfd998
DOGHOUSETV <arubigeorge0@gmail.com>
2025-07-03 19:06:36 -0400

    resolve id promise before fetching card data

--- src/app/business-card/[id]/page.tsx
+++ src/app/business-card/[id]/page.tsx
@@ -1,7 +1,12 @@
 import { getCardData } from "@/lib/getCardData"
 import CardPageContent from "./CardPageContent"
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const data = await getCardData(params.id)
-  return <CardPageContent data={data} id={params.id} />
+export default async function Page({ 
+  params 
+}: { 
+  params: Promise<{ id: string }> 
+}) {
+  const resolvedParams = await params
+  const data = await getCardData(resolvedParams.id)
+  return <CardPageContent data={data} id={resolvedParams.id} />
 }
