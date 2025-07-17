4455d8144b4e8f3deddbf6926972d9c3515bb403
Rick Markowitz <rick@fanslove.com>
2025-07-17 15:54:09 -0700

    updated ENV variable name

--- src/lib/getCardData.ts
+++ src/lib/getCardData.ts
@@ -69,7 +69,7 @@ export async function getCardData(
   custom_endpoint: string
 ): Promise<CardData | null> {
   try {
-    const url = `${process.env.NEXT_PATCHBAY_API_URL}/api/v1/card/public/${custom_endpoint}`;
+    const url = `${process.env.NEXT_PUBLIC_PATCHBAY_API_URL}/api/v1/card/public/${custom_endpoint}`;
     const res = await fetch(url);
     if (!res.ok) {
       return null;
